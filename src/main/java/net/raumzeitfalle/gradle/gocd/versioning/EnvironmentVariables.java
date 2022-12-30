ec066d92e5f755d80b874de09aabad8116e29291
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2022-12-30 20:37:39 +0100

    Added TODO.

--- src/main/java/net/raumzeitfalle/gradle/gocd/versioning/EnvironmentVariables.java
+++ src/main/java/net/raumzeitfalle/gradle/gocd/versioning/EnvironmentVariables.java
@@ -1,13 +1,19 @@
 package net.raumzeitfalle.gradle.gocd.versioning;
 
 public enum EnvironmentVariables {
-    GO_PIPELINE_COUNTER,
+    GO_SERVER_URL,
+    
+    GO_TRIGGER_USER,
+    
     GO_PIPELINE_NAME,
+    GO_PIPELINE_COUNTER,
     GO_PIPELINE_LABEL,
+    
     GO_STAGE_NAME,
     GO_STAGE_COUNTER,
-    GO_SERVER_URL,
+    
     GO_JOB_NAME,
-    GO_TRIGGER_USER,
+    
+    // TODO: this exists on windows. How about mac or linux?
     COMPUTERNAME;
 }
